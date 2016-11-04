d36472c8b5a9ff458646678a4acf046793aec545
Shobhna Garg <Shobhna Garg>
2016-11-04 12:43:31 +0530

    Add existing file

---
+++ src/test/java/com/quaatso/mavenDemo/TestNgDemo.java
@@ -0,0 +1,17 @@
+package com.quaatso.mavenDemo;
+
+import org.testng.annotations.Test;
+
+
+
+
+
+public class TestNgDemo {
+	
+	@Test
+	public void firstTest(){
+		
+		System.out.println("inside first test");
+	}
+
+}
